e0a375a2cea44b5787ff5c20fcc32f17bb3ce10b
Anand Inamdar <inamdaranand6@gmail.com>
2026-06-14 18:44:27 +0530

    Added Jasmine tests

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
+const calculateSimpleInterest = (p, r, t) => {
+  return (p * r * t) / 100;
+};
+
+const calculateTotalPayableAmount = (p, simpleInterest) => {
+  return p - simpleInterest;
+};
+
 const calculate = () => {
   let p = Number(document.getElementById("principal").value);
   let r = Number(document.getElementById("rate").value);
   let t = Number(document.getElementById("time").value);
-  let simpleInterest = (p * r * t) / 100;
-  let amount = p + simpleInterest;
+
+  let simpleInterest = calculateSimpleInterest(p, r, t);
+  let amount = calculateTotalPayableAmount(p, simpleInterest);
 
   let result = document.getElementById("result");
 
@@ -11,3 +20,6 @@ const calculate = () => {
   <div>Total Interest: <span>${simpleInterest.toFixed(2)}</span></div>
   <div>Total Amount: <span>${amount.toFixed(2)}</span></div>`;
 };
+
+if (typeof module !== 'undefined')
+  module.exports = { calculateSimpleInterest, calculateTotalPayableAmount, calculate };
---
+++ spec/scriptSpec.js
@@ -0,0 +1,13 @@
+const { calculateSimpleInterest, calculateTotalPayableAmount } = require('../script');
+
+describe("Interest Rate Calculator Tests", () => {
+
+  it("should calculate simple interest correctly", () => {
+    expect(calculateSimpleInterest(1000, 5, 1)).toBe(50);
+  });
+
+  it("should calculate total payable amount correctly", () => {
+    expect(calculateTotalPayableAmount(1000, 50)).toBe(1050);
+  });
+
+});
